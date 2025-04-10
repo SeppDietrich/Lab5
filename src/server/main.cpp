6350085f3bb4d8362ad47c533866471c192ea0fe
Nicolae <nicolae.iurcu@colegiu.utm.md>
2025-04-10 21:18:46 +0300

    Commit

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,41 +1,70 @@
-#include <cstring>
 #include <iostream>
-#include <netinet/in.h>
-#include <sys/socket.h>
+#include <cstring>
+#include <cstdlib>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <thread>
+#include <vector>
 
-using namespace std;
+#define PORT 8080
+#define MAX_CLIENTS 10
 
-int main()
-{
-    // creating socket
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+Class Client{
 
-    // specifying the address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+}
+void handleClient(int clientSocket) {
+    char buffer[1024];
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+        int bytesRead = read(clientSocket, buffer, sizeof(buffer));
+        if (bytesRead <= 0) {
+            std::cout << "Client disconnected." << std::endl;
+            break;
+        }
+        std::cout << "Client: " << buffer << std::endl;
+        send(clientSocket, buffer, bytesRead, 0);
+    }
+    close(clientSocket);
+}
 
-    // binding socket.
-    bind(serverSocket, (struct sockaddr*)&serverAddress,
-         sizeof(serverAddress));
+int main() {
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t addrLen = sizeof(clientAddr);
 
-    // listening to the assigned socket
-    listen(serverSocket, 5);
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket < 0) {
+        std::cerr << "Socket creation failed." << std::endl;
+        return -1;
+    }
 
-    // accepting connection request
-    int clientSocket
-        = accept(serverSocket, nullptr, nullptr);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
 
-    // recieving data
-    char buffer[1024] = { 0 };
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout << "Message from client: " << buffer
-              << endl;
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        std::cerr << "Bind failed." << std::endl;
+        return -1;
+    }
 
-    // closing the socket.
+    if (listen(serverSocket, MAX_CLIENTS) < 0) {
+        std::cerr << "Listen failed." << std::endl;
+        return -1;
+    }
+
+    std::cout << "Server is listening on port " << PORT << std::endl;
+
+    while (true) {
+        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
+        if (clientSocket < 0) {
+            std::cerr << "Accept failed." << std::endl;
+            continue;
+        }
+        std::cout << "New client connected." << std::endl;
+
+        std::thread(handleClient, clientSocket).detach();
+    }
+
     close(serverSocket);
-
     return 0;
 }
