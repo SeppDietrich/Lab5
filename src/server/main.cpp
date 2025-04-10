f52e2d6613978e3531f0de95919025d31716e806
Nicolae <nicolae.iurcu@colegiu.utm.md>
2025-04-10 23:05:30 +0300

    Commit

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -57,7 +57,7 @@ void handleClient(int clientSocket) {
             std::cout << "Client disconnected." << std::endl;
             break;
         }
-        const *char Username =client.getUsernameString();
+        const* char Username =client.getUsernameString();
         buffer =Username.c_str() + " : " + buffer
         std::cout << client.getUsernameString()<<" : " << buffer << std::endl;
         send(clientSocket, buffer, bytesRead, 0);
