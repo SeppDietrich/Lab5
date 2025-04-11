528c446f59cccfe9c0c1d9488d4164a3fe5738ab
Nicolae <nicolae.iurcu@colegiu.utm.md>
2025-04-11 23:43:45 +0300

    Commit

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -7,17 +7,187 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include "User.h"
-#include "ChatRoom.h"
-#include "RoomHandler.h"
+// #include "User.h"
+// #include "ChatRoom.h"
+// #include "RoomHandler.h"
 
 #define PORT 8080
 #define MAX_CLIENTS 10
 
 
+class RoomHandler {
+private:
+    std::vector<ChatRoom> rooms;
+
+    void createRoom(int roomId) {
+        rooms.emplace_back(roomId); 
+
+    }
+
+public:
+    
+    void joinRoom(User* user, int roomId) {
+        ChatRoom* roomToJoin = nullptr;
+        
+        // Find existing room
+        for(auto& room : rooms) {
+            if(room.getId() == roomId) {
+                roomToJoin = &room;
+                break;
+            }
+        }
+        
+        // Create new room if not found
+        if(!roomToJoin) {
+            rooms.emplace_back(roomId);
+            roomToJoin = &rooms.back();
+        }
+        
+        roomToJoin->addChater(user);
+
+    }
+    
+};
+
+class ChatRoom{
+private:
+    std::vector<User*> chaters;
+    int id;
+    
 
 
+public:
+    ChatRoom(int id){
+        this->id=id;
+    }
+    
+    ~ChatRoom() = default;
+    int getId(){return id;}
+    void addChater(User* user){
+        chaters.push_back(user);
+        user->setCurrentRoom(this);
+    }
+    void broadCastInRoom(std::string Username, std::string message){
+        std::string fullMessage=Username+" : "+message+"\n";
+        for(int i=0;i<chaters.size();i++){
+            chaters[i]->sendMessage(fullMessage);
+        }
+       
+    }
+};
 
+class User{
+private:
+    RoomHandler* roomHandler;
+    int socket;
+    std::string Username;
+    ChatRoom* currentRoom;
+
+public:
+    User(int& socket, RoomHandler& rh) : 
+        socket(socket), 
+        roomHandler(rh),
+        currentRoom(nullptr) {}
+    ~User() {}  
+
+    ChatRoom* getCurrentRoom() const {
+        return currentRoom;
+    }
+
+    void User::setCurrentRoom(ChatRoom* room) {
+        currentRoom = room;
+    }
+
+    void sendMessage(const std::string& message){
+        int bytesSent = send(socket, message.c_str(), message.size(), 0);  
+        if (bytesSent < 0) {
+            std::cerr << "Error sending message to client" << std::endl;
+        }  
+    }
+    
+    bool authenticationSuccess(std::string sentUsername) {  
+        
+        if(sentUsername.length()>100){
+            return false;
+        }
+        Username = sentUsername;  
+        std::cout<<"Client authenticated as : " <<Username<<std::endl;
+        return true;
+    }
+
+    void handleClient() {
+        char buffer[1024];
+
+        while (true) {
+            memset(buffer, 0, sizeof(buffer));
+            int bytesRead = read(socket, buffer, sizeof(buffer));
+            if (bytesRead <= 0) {
+                std::cout << "Client disconnected." << std::endl;
+                break;
+            }
+            buffer[bytesRead] = '\0';
+
+            
+            // Trim any trailing newlines or whitespace
+            std::string message(buffer);
+            
+            
+            
+            std::string fullMessage = parseMessage(message);
+            
+            //std::cout << fullMessage << "\n";
+            
+            sendMessage(fullMessage); 
+        }
+        close(socket);
+    }
+private:
+
+    std::string parseMessage(std::string message){
+        message.erase(message.find_last_not_of(" \t\n\r\f\v") + 1);
+        if(message[0]!='/'){
+            std::string manual=
+                "/auth [username] - authenticate \n"
+                "/join [chatNumber] - join chat \n"
+                "/message [message] -message sent into chat \n"
+                "/leave -leave chat \n";
+            
+            return manual;
+        }
+        int spacePos = message.find(' ');
+        std::string command = message.substr(0, spacePos);
+        std::string data = (spacePos != std::string::npos) ? message.substr(spacePos + 1) : "";
+
+        
+        switch(command[1]) {
+            case 'a':{ // /auth
+                if(authenticationSuccess(data)) {
+                    return "Successfully logged in as " + data + "\n";
+                }
+                return "Authentication Failed";
+            }
+                
+            case 'j':{ // /join
+                int roomId=std::stoi(data);
+                roomHandler.joinRoom(this, data);
+
+                return "Joining chat: " + data;
+            }
+            case 'm':{ // /message
+                currentRoom->broadCastInRoom(Username, data);
+                return "Sending message: " + data;
+            }
+            case 'l':{ // /leave
+                return "Leaving current chat";
+            }
+            default:{
+                return "Unknown command: " + command;
+            }
+        }
+
+        return "Unknown command";
+    }
+};
 RoomHandler roomHandler;
 
 
