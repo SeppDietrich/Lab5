d6db90daf9763685ba620d04524538aa4cfd0e6c
Nicolae <nicolae.iurcu@colegiu.utm.md>
2025-04-12 13:08:12 +0300

    Commit

--- server.sh
+++ server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git pull
 
-g++ -std=c++11 src/server/main.cpp src/server/User.cpp src/server/ChatRoom.cpp src/server/RoomHandler.cpp -o server -lpthread
-
+# g++ -std=c++11 src/server/main.cpp src/server/User.cpp src/server/ChatRoom.cpp src/server/RoomHandler.cpp -o server -lpthread
+g++ -std=c++17 -Wall -Wextra -I./include src/server/*.cpp src/main.cpp -o server
 ./server
 
 rm server
